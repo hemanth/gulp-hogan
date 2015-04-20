a1fe6dd78de791368b36502d563b9c951c93b2bb
gregory maertens <greg@conjurelabs.com>
2015-04-20 17:01:06 +0200

    added support to options (passed to Hogan as second argument in Hogan.compile()). added optional extension (to support .html output)

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var es = require('event-stream');;
 var gutil = require('gulp-util');
 var Hogan = require('hogan.js');
 
-module.exports = function(data) {
+module.exports = function(data, options, extension) {
   data = data || {};
+  extension = extension || '.js';
   return es.map(function (file, cb) {
-    file.contents = new Buffer( Hogan.compile(file.contents.toString()).render(data) );
-    file.path = gutil.replaceExtension(file.path, '.js');
+    file.contents = new Buffer( Hogan.compile(file.contents.toString(), options).render(data) );
+    file.path = gutil.replaceExtension(file.path, extension);
     cb(null,file);
   });
 };
--- test.js
+++ test.js
@@ -3,6 +3,14 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var hogan = require('./index');
 
+function fixture(stream, content)
+{
+    stream.write(new gutil.File({
+                                path: 'fixture.hogan',
+                                contents: new Buffer(content)
+                                }));
+}
+
 it('should compile hogan to js', function (cb) {
         var stream = hogan({handle: 'gnumanth'});
 
@@ -11,9 +19,27 @@ it('should compile hogan to js', function (cb) {
                 assert.equal(file.contents.toString(), 'gnumanth');
                 cb();
         });
-
-        stream.write(new gutil.File({
-                path: 'fixture.hogan',
-                contents: new Buffer('{{handle}}')
-        }));
+        fixture(stream, '{{handle}}');
 });
+
+it('should support custom delimiters', function (cb) {
+   var stream = hogan({handle: 'gnumanth'}, {delimiters: '<% %>'});
+   stream.on('data', function (file) {
+             assert.equal(file.relative, 'fixture.js');
+             assert.equal(file.contents.toString(), 'gnumanth');
+             cb();
+             });
+   fixture(stream, '<% handle %>');
+   });
+
+it('should support custom extensions', function (cb) {
+   var stream = hogan({handle: 'gnumanth'}, null, '.html');
+   
+   stream.on('data', function (file) {
+             assert.equal(file.relative, 'fixture.html');
+             assert.equal(file.contents.toString(), 'gnumanth');
+             cb();
+             });
+   
+   fixture(stream, '{{handle}}');
+   });
